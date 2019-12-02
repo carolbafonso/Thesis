0bd9dd1ff58b3d34e27e555ac70eb509810539e1
carolbafonso <carolina_afonso4@hotmail.com>
2019-12-02 17:23:22 +0000

    first try to cut the signal

--- Lead_converter.py
+++ Lead_converter.py
@@ -4,7 +4,6 @@ import glob
 import numpy as np
 from scipy.signal import butter, lfilter, find_peaks
 
-
 def butter_lowpass(cutoff, fs, order=5):
     nyq = 0.5 * fs
     normal_cutoff = cutoff / nyq
@@ -18,11 +17,21 @@ def butter_lowpass_filter(data, cutoff, fs, order):
 
 def findPeaks(signal):
     find_peaks(signal, height=0)
-    peaks, _ = find_peaks(signal, height=0)
-    plt.plot(signal, color='black')
-    plt.plot(peaks, signal[peaks], 'o', color='red')
+    peaks, _ = find_peaks(signal)
+    plt.plot(signal, color='grey')
+    plt.plot(peaks, signal[peaks], "x")
+   # plt.plot(peaks, signal[peaks], 'o', color='red')
     plt.show()
-
+    
+def cut_signal(mat,i):
+    sig = mat[0]['val'][i][2000:2750]
+    sig_f = butter_lowpass_filter(sig, 2.5, 100.0, 5) 
+    findPeaks(sig_f)
+    #plt.plot(sig_f_p, color='grey')
+    #plt.show()
+    
+   
+        
 
 files = [file for file in glob.glob('PTB/PTB/*.mat')]
 mat = []
@@ -37,6 +46,16 @@ high = 1250.0
 i = 0
 dim = len(mat[0]['val'])
 
+"""
+sig = mat[0]['val'][1][2000:4000]
+fig, (ax1, ax2) = plt.subplots(1, 2, sharey=True, figsize=(20,10))
+ax = plt.axhline(y=0, color='r', linestyle='-')
+filtered = butter_lowpass_filter(sig, 2.5, 100.0, 5)
+ax1.plot(np.linspace(10,15,num=len(sig)), sig+500, color='lightblue', label='Normal Signal')
+ax2 = findPeaks(filtered)
+cut(mat)
+"""
+
 
 while i < dim:
     sig = mat[0]['val'][i][2000:4000]
@@ -45,4 +64,5 @@ while i < dim:
     filtered = butter_lowpass_filter(sig, 2.5, 100.0, 5)
     ax1.plot(np.linspace(10,15,num=len(sig)), sig+500, color='lightblue', label='Normal Signal')
     ax2 = findPeaks(filtered)
+    cut(mat,i)
     i += 1
