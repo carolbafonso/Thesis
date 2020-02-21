365d74efe55a2fe5af0bcd1ebf894dd94a10df05
carolbafonso <49485222+carolbafonso@users.noreply.github.com>
2020-02-21 11:45:19 +0000

    Add files via upload
    start sorting train samples and test samples

--- Lead_converter.py
+++ Lead_converter.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 import scipy.io
 import glob
 import numpy as np
+import sys
 
 from scipy.signal import butter, lfilter, filtfilt
 from detectors_mod import Detectors
@@ -50,17 +51,15 @@ def butter_highpass_filter(data, cutoff, fs, order=5):
 # PEAKS #
 def cut_signal(np_array, peaks, fs):
     #recive signal with peak r detected
-    #cut window [p_max-0.2:p_max+0.4]
-    
-<<<<<<< HEAD
+
     sig = np.zeros((len(peaks), 650))
     
     for pp in range(len(peaks)):
         p_max = peaks[pp]
-=======
+
     for pp in peaks:
         p_max = pp
->>>>>>> eda566e5be7b56dcb6c045496fcbbcbccb68faa3
+
         d = len(np_array)
         l = int(p_max - fs*0.2)
         h = int(p_max + fs*0.45)
@@ -68,15 +67,16 @@ def cut_signal(np_array, peaks, fs):
         print("h1=" , h)
         print("array[0] = ", np_array)
         
-<<<<<<< HEAD
-        if l >= 0 and h < d:
-            sig[pp] = np_array[l:h]
-            #TODO: Normalizaçao [-1, 1]
-            #print("sig=", sig)
-            plt.plot(np.linspace(l, h, num=len(sig)), sig)
-            plt.show()
+        if l > np_array[0] or h < np_array[d-1]:
+           sig = np_array[l:h]
+           #print("sig=", sig)
+           b = 2*(sig - np.min(sig))/(np.max(sig) - np.min(sig)) - 1
+           plt.plot(np.linspace(l, h, num=len(b)), b)
+           plt.show()
+             
     return sig
 
+    
 ################            
 # AUTO-ENCODER #
 ################
@@ -93,12 +93,12 @@ def autoencoder(M, X_train):
     y = Dense(M, activation='tanh')(f)
     
     model = Model(x,y)
-    optim = optimizers.Adam(lr=0.00001)
+    optim = optimizers.Adam(lr=0.0001)
     model.compile(loss='mean_squared_error', optimizer=optim)
     
     model.summary()
     
-    history = model.fit(X_train, X_train, batch_size=16, epochs=200, verbose=1, validation_split=0.2)
+    history = model.fit(X_train, X_train, batch_size=32, epochs=200, verbose=1, validation_split=0.2)
     
     plt.figure()
     x_aux = range(1, len(history.history['loss'])+1)
@@ -112,14 +112,7 @@ def autoencoder(M, X_train):
     plt.show()
     
     return model, history
-=======
-        if l > np_array[0] or h < np_array[d-1]:
-            sig = np_array[l:h]
-            #print("sig=", sig)
-            plt.plot(np.linspace(l, h, num=len(sig)), sig)
-            plt.show()
-            
->>>>>>> eda566e5be7b56dcb6c045496fcbbcbccb68faa3
+
 
 ########
 # MAIN #     
@@ -128,63 +121,63 @@ def autoencoder(M, X_train):
 #data
 files = [file for file in glob.glob('PTB/PTB/*.mat')]
 mat = []
-for f in files:
-    pf = scipy.io.loadmat(f)
-    mat.append(pf)
 
 #filter
 fs = 1000
 low = 1
 high = 40
-dim = len(mat[0]['val'])
 
-X_train = []
+
+X_train = list()
+X_test = list()
 
 detectors = Detectors(fs)
 
-<<<<<<< HEAD
-i=0
 #FOR EVERY FILE:   <<<<<<<<<
 
 # FIRST: DETECT PEAKS IN LEAD 1
 # SECOND: CUT HEARTBEATS IN EVERY LEAD AT LOCATIONS DETECTED AT LEAD 1
-while i < 1:
-    sig = mat[0]['val'][i] #mat[sample]['val'][lead][window]
-    #fig, (ax1, ax2, ax3, ax4) = plt.subplots(1, 4, sharey=True, figsize=(10,5))
-=======
-      
-
-while i < dim:
-    sig = mat[0]['val'][i][2000:4000] #mat[sample]['val'][lead][window]
-    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, sharey=True, figsize=(10,5))
+for f in files:
+    pf = scipy.io.loadmat(f)
+    mat.append(pf)
+    dim = len(mat[0]['val'])
+    i = 0
+    while i < dim:
+        sig = mat[0]['val'][i] #mat[sample]['val'][lead][window]
     
-    Low_filt = butter_lowpass_filter(sig, high, fs, 5)
-    High_filt = butter_highpass_filter(Low_filt, low, fs, 5) 
->>>>>>> eda566e5be7b56dcb6c045496fcbbcbccb68faa3
-    
-    Low_filt = butter_lowpass_filter(sig, high, fs, 5)
-    High_filt = butter_highpass_filter(Low_filt, low, fs, 5) 
+        fig, (ax1, ax2, ax3) = plt.subplots(1, 3, sharey=True, figsize=(10,5))
         
-    #ax1.plot(np.linspace(0,len(sig),num=len(sig)), sig, color='lightblue', label='Normal Signal')
-    #ax2.plot(np.linspace(0,len(sig),num=len(High_filt)), High_filt, color='black', label='Filtered signal')
-    
-    r_peaks = detectors.pan_tompkins_detector(sig)
-    print("r_peaks=", r_peaks)
-    
-    r_peaks = np.array(r_peaks)
-    
-    ax3 = cut_signal(High_filt,r_peaks, fs) 
-    
-    sig_train = 2*(ax3 - np.min(ax3))/(np.max(ax3) - np.min(ax3)) - 1
-    
-    # X_train = np.concatenate((X_train, ax3))   <<<<
+        Low_filt = butter_lowpass_filter(sig, high, fs, 5)
+        High_filt = butter_highpass_filter(Low_filt, low, fs, 5) 
+            
+        #ax1.plot(np.linspace(0,len(sig),num=len(sig)), sig, color='lightblue', label='Normal Signal')
+        #ax2.plot(np.linspace(0,len(sig),num=len(High_filt)), High_filt, color='black', label='Filtered signal')
+        
+        r_peaks = detectors.pan_tompkins_detector(sig)
+        print("r_peaks_lead1=", r_peaks)
+        r_peaks = np.array(r_peaks)
+        ax3 = cut_signal(High_filt,r_peaks, fs) 
+        
+        if(len(X_train) == 200):
+            X_test.append(pf)
+        else:
+            X_train.append(ax3)  
+        
+        i += 1        
+
+print("X_train", len(X_train))
+print("X_test", len(X_test))
     
 # NO fim de ir buscar picos a todos os ficheiros:
-    X_train = sig_train
+ 
+sys.exit()
 
-    print(X_train.shape)
-    model, history = autoencoder(len(X_train[1]), X_train)
+X_train = np.array(X_train)
+print(X_train.shape)
+
+model, history = autoencoder((len(X_train[0])), X_train)
+
     
-    print(history.history['loss'])
+print(history.history['loss'])
     
-    i += 1
+
