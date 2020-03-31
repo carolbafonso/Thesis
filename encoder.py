daa3bff36675bb50a01fdff0f3e27a19a4ed6d24
carolbafonso <49485222+carolbafonso@users.noreply.github.com>
2020-03-31 17:52:18 +0100

    Add files via upload
    normalization / autoencoder add layers / first result

--- encoder.py
+++ encoder.py
@@ -10,20 +10,20 @@ import matplotlib.pyplot as plt
 import pickle
 import sys
 import numpy as np
-import keras.backend as K
 
 from keras.layers import Dense, Input
 from keras.models import Model
 from keras import optimizers
 from scipy.signal import butter, lfilter, filtfilt
 
-def butter_lowpass(cutoff, fs, order=5):
+
+def butter_lowpass(cutoff, fs, order=3):
     nyq = 0.5 * fs
     normal_cutoff = cutoff / nyq
     b, a = butter(order, normal_cutoff, btype='low', analog=False)
     return b, a
 
-def butter_lowpass_filter(data, cutoff, fs, order=5):
+def butter_lowpass_filter(data, cutoff, fs, order=3):
     
     b, a = butter_lowpass(cutoff, fs, order=order)
     y = lfilter(b, a, data)
@@ -41,6 +41,7 @@ def butter_highpass_filter(data, cutoff, fs, order=5):
     y = filtfilt(b, a, data, padlen=0)
     return y
 
+
 ################            
 # AUTO-ENCODER #
 ################
@@ -49,32 +50,24 @@ def autoencoder(M, X_train):
     print("M=", M)
     x = Input(shape=(M,))
     print("x=", x)
-    b = Dense(int(M/2), activation='tanh')(x)
-    c = Dense(int(M/3), activation='tanh')(b)
-    d = Dense(int(M/6), activation='tanh')(c)
-    e = Dense(int(M/3), activation='tanh')(d)
-    f = Dense(int(M/2), activation='tanh')(e)
-    y = Dense(M, activation='tanh')(f)
-
-    def mean_pred(y_true, y_pred):
-        return K.mean(y_pred)
+    b = Dense(int(M/3), activation='tanh')(x)
+    c = Dense(int(M/6), activation='tanh')(b)
+    d = Dense(int(M/9), activation='tanh')(c)
+    e = Dense(int(M/12), activation='tanh')(d)
+    f = Dense(int(M/9), activation='tanh')(e)
+    g = Dense(int(M/6), activation='tanh')(f)
+    h = Dense(int(M/3), activation='tanh')(g)
+    y = Dense(M, activation='tanh')(h)
 
     model = Model(x,y)
-    optim = optimizers.Adam(lr=0.0005)
-    model.compile(loss='mean_squared_error', optimizer=optim, metrics=['accuracy', mean_pred])
+    optim = optimizers.Adam(lr=0.00001)
+    model.compile(loss='mean_squared_error', optimizer=optim)
     
     predict = model.predict(X_train)
-
-    Low_filt = butter_lowpass_filter(predict, 1, 1000, 5)
-    High_filt = butter_highpass_filter(Low_filt, 40, 1000, 5)    
-    
-    fig, (ax1) = plt.subplots(1, 1, sharey=True, figsize=(10,5))
-    ax1.plot(np.linspace(0,len(High_filt),num=len(High_filt)), High_filt, color='lightblue', label='Normal Signal')
-    
     model.summary()
     
     history = model.fit(X_train, X_train, batch_size=32, epochs=200, verbose=1, validation_split=0.2)
-    
+        
     plt.figure()
     x_aux = range(1, len(history.history['loss'])+1)
     train, = plt.plot(x_aux, history.history['loss'], 'b', label = 'Train')
@@ -85,19 +78,9 @@ def autoencoder(M, X_train):
     plt.xlabel('Epoch')
     plt.ylabel('Loss')
     plt.show()
+
+    return predict
     
-    plt.figure()
-    x_aux = range(1, len(history.history['accuracy'])+1)
-    train, = plt.plot(x_aux, history.history['accuracy'], 'green', label = 'Train')
-    plt.ylim(ymin=0.0)
-    plt.legend(handles=[train])
-    plt.grid()
-    plt.title('Accuracy History')
-    plt.xlabel('Epoch')
-    plt.ylabel('accuracy')
-    plt.show() 
-    
-    return model, history
 
 
 ########            
@@ -108,16 +91,21 @@ filename = 'data_1.pk'
 
 with open(filename, 'rb') as hf:
     data = pickle.load(hf)
-    
 
 for i in data[0]:
     j = 0
-    #while j < 15:
-    M = len(data[0]['0'][0]) #data[{train}, {test}][lead][vetor da lead]
-    Train = data[0]['0']
-    plt.plot(data[0]['0'][0])
-    plt.show()
-    model, history = autoencoder(M, Train)
-    sys.exit()
+    while j < 15:
+        M = len(data[0]['0'][0]) #data[{train}, {test}][lead][vetor da lead]
+        Train = data[0]['0']
+        Train = 2*(Train-np.min(Train))/(np.max(Train)-np.min(Train))-1
+        plt.plot(data[0]['0'][0])
+        plt.show()
+        predict = autoencoder(M, Train)
+        Low_filt = butter_lowpass_filter(predict[0], 40, 1000, 5)
+        High_filt = butter_highpass_filter(Low_filt, 1, 1000, 5)
+        plt.plot(High_filt, color= 'black')
+        plt.show()
+        sys.exit()
+       
     #SVG(model_to_dot(model).create(prog='dot', format='svg'))
     
